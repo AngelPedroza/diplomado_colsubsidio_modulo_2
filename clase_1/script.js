ea5940162a1d9d269aba518814d6a0f0f2ea1771
AngelPedroza <angel.pedroza@rappi.com>
2022-10-21 18:27:59 -0500

    commit last class

---
+++ clase_1/script.js
@@ -0,0 +1,29 @@
+// Condiciones
+
+// 1: No pueden usar if, else, else if
+// 2: No pueden usar var
+// 3: Solo puede usar una unica vez console.log
+// 4: Deben usar un loop while o un loop for
+
+const primera = (data) => {
+    console.log("Hola")
+} 
+
+
+
+// true: omar, miguel, sandra, freddy, espejo, penagos, ronald
+// false: luis, angel
+
+// boleano: freddy, sandra, luis, miguel, carlos penagos
+
+// let elem = "hola"
+
+//5, 7, 9 // numeros
+
+// "hola", "pepito", "perez" // strings
+
+// true, false // boleanos
+
+// variables => let elemento; // undefined
+
+// {nombre: "pedro", edad: 5} // objetos objects
